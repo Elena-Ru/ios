f8ebbf2a104ae08db1a88ec9922e385eb6091cb7
Elena Russkykh <lenarusskih@gmail.com>
2022-06-14 13:19:23 +0200

    Змейка

---
+++ Snake_lesson_8_1/Snake_lesson_8_1/Frame.swift
@@ -0,0 +1,15 @@
+//
+//  Frame.swift
+//  Snake_lesson_8_1
+//
+//  Created by Елена Русских on 14.06.2022.
+//
+
+//import UIKit
+//import SpriteKit
+//
+//class Frame: SKShapeNode{
+//    path = UIBezierPath(rect: CGRect(x: -100, y: 20, width: 200, height: 400)).cgPath
+//    
+//    
+//}

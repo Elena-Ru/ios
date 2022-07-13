c18b6b6528f5401a1139785c3cb3e2c0b9bb5357
Elena Russkykh <lenarusskih@gmail.com>
2022-07-13 09:42:41 +0200

    Домашняя работа к уроку 9

---
+++ VKontakte/VKontakte/Model/Group.swift
@@ -0,0 +1,20 @@
+//
+//  Group.swift
+//  VKontakte
+//
+//  Created by Елена Русских on 20.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class Group {
+    
+    let image: UIImage
+    let name: String
+    
+ init (name: String, image: UIImage) {
+        self.image = image
+        self.name = name
+    }
+}
